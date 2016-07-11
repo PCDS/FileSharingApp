36d1245e2a19f7b88dc1e4af08a0bffa4876c781
Blake J. Wrege <bwrege@thermaltech.com>
2016-07-11 15:53:47 -0400

    fixed updater bug deleting Server.ini

--- Updater.bat
+++ Updater.bat
@@ -4,5 +4,5 @@ mkdir Releases
 Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/PCDS/FileSharingApp/zipball/master', 'Releases\currentversion.zip'); "
 Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://pcds.github.io/FileSharingApp/versioninfo.html', 'Releases\currentversion.txt'); "
 Powershell.exe  -ExecutionPolicy Bypass "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('Releases\currentversion.zip', 'Releases\currentversion'); "
-Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\* -exclude 'Server.ini.ini','Client.ini','Updater.bat','Releases' "
+Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\* -exclude 'Server.ini','Client.ini','Updater.bat','Releases' "
 start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\PCDS*\* -Destination . -Recurse -force "
