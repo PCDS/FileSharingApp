d75ae357f2414a060ee2d57ab73e118580c5f8ae
Blake J. Wrege <bwrege@thermaltech.com>
2016-07-11 15:50:20 -0400

    made improvements to the updater

--- Updater.bat
+++ Updater.bat
@@ -1,7 +1,8 @@
 
 IF EXIST Releases Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\Releases -Force -Recurse"
 mkdir Releases
-copy Updater.config Releases
-Powershell.exe  -ExecutionPolicy Bypass -Command - <Releases\Updater.config
-del Releases\Updater.config
+Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/PCDS/FileSharingApp/zipball/master', 'Releases\currentversion.zip'); "
+Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://pcds.github.io/FileSharingApp/versioninfo.html', 'Releases\currentversion.txt'); "
+Powershell.exe  -ExecutionPolicy Bypass "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('Releases\currentversion.zip', 'Releases\currentversion'); "
+Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\* -exclude 'Server.ini.ini','Client.ini','Updater.bat','Releases' "
 start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\PCDS*\* -Destination . -Recurse -force "
