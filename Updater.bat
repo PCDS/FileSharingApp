32c509b53a6dffb23263b50af549c225da0e0461
Blake J. Wrege <bwrege@thermaltech.com>
2016-08-23 17:36:34 -0400

    Working Alpha

--- Updater.bat
+++ Updater.bat
@@ -1,9 +1,9 @@
 Taskkill /IM FileSharingAppClient.exe /F
 Taskkill /IM FileSharingAppServer.exe /F
-cd %~dp0
 IF EXIST Releases Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\Releases -Force -Recurse"
 mkdir Releases
-Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/PCDS/FileSharingApp/archive/master.zip', 'Releases\currentversion.zip'); "
-Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\* -exclude 'Server.ini','Client.ini','Updater.bat','currentversion.zip','Users.sqlite','ReceivedFiles' -Recurse "
+Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://github.com/PCDS/FileSharingApp/zipball/master', 'Releases\currentversion.zip'); "
+Powershell.exe  -ExecutionPolicy Bypass "(New-Object System.Net.WebClient).DownloadFile('https://pcds.github.io/FileSharingApp/versioninfo.html', 'Releases\currentversion.txt'); "
 Powershell.exe  -ExecutionPolicy Bypass "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('Releases\currentversion.zip', 'Releases\currentversion'); "
-start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\FileSharingApp-master\* -Destination . -Recurse -force; start cmd.exe ' /c echo UPDATING IS COMPLETE & pause '"
+Powershell.exe  -ExecutionPolicy Bypass "Remove-Item .\* -exclude 'Server.ini','Client.ini','Updater.bat','Releases','Users.sqlite','ReceivedFiles' "
+start Powershell.exe -ExecutionPolicy Bypass "Copy-Item  -Path Releases\currentversion\PCDS*\* -Destination . -Recurse -force; start cmd.exe ' /c echo WORKBOOK IS DONE INSTALLING, YOU MAY NOW DELETE THIS FOLDER & pause '"
