76862a3119c69ed30f39ea63ae91fb431f87841c
Blake J. Wrege <bwrege@thermaltech.com>
2016-08-23 17:39:40 -0400

    added server code

---
+++ Genome-Server-Normal/MainForm.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace httpMethodsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdModifiedHeaders = new System.Windows.Forms.RadioButton();
+            this.rdStandardHeaders = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChatPort = new System.Windows.Forms.TextBox();
+            this.ipAddress = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lbIp = new System.Windows.Forms.Label();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.settingsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveConfigurationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadConfigurationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.createUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox2.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(64, 103);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(239, 27);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Select Data File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(64, 136);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(239, 27);
+            this.btnStartServer.TabIndex = 4;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdModifiedHeaders);
+            this.groupBox2.Controls.Add(this.rdStandardHeaders);
+            this.groupBox2.Location = new System.Drawing.Point(326, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 93);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Response Headers";
+            // 
+            // rdModifiedHeaders
+            // 
+            this.rdModifiedHeaders.AutoSize = true;
+            this.rdModifiedHeaders.Location = new System.Drawing.Point(6, 66);
+            this.rdModifiedHeaders.Name = "rdModifiedHeaders";
+            this.rdModifiedHeaders.Size = new System.Drawing.Size(65, 17);
+            this.rdModifiedHeaders.TabIndex = 1;
+            this.rdModifiedHeaders.Text = "Modified";
+            this.rdModifiedHeaders.UseVisualStyleBackColor = true;
+            this.rdModifiedHeaders.CheckedChanged += new System.EventHandler(this.rdModifiedHeaders_CheckedChanged);
+            // 
+            // rdStandardHeaders
+            // 
+            this.rdStandardHeaders.AutoSize = true;
+            this.rdStandardHeaders.Checked = true;
+            this.rdStandardHeaders.Location = new System.Drawing.Point(6, 31);
+            this.rdStandardHeaders.Name = "rdStandardHeaders";
+            this.rdStandardHeaders.Size = new System.Drawing.Size(68, 17);
+            this.rdStandardHeaders.TabIndex = 0;
+            this.rdStandardHeaders.TabStop = true;
+            this.rdStandardHeaders.Text = "Standard";
+            this.rdStandardHeaders.UseVisualStyleBackColor = true;
+            this.rdStandardHeaders.CheckedChanged += new System.EventHandler(this.rdStandardHeaders_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Chat Listening Port";
+            // 
+            // txtChatPort
+            // 
+            this.txtChatPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChatPort.Location = new System.Drawing.Point(162, 67);
+            this.txtChatPort.Name = "txtChatPort";
+            this.txtChatPort.Size = new System.Drawing.Size(100, 20);
+            this.txtChatPort.TabIndex = 35;
+            this.txtChatPort.Text = "1986";
+            this.txtChatPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ipAddress
+            // 
+            this.ipAddress.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ipAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipAddress.Location = new System.Drawing.Point(162, 48);
+            this.ipAddress.Name = "ipAddress";
+            this.ipAddress.ReadOnly = true;
+            this.ipAddress.Size = new System.Drawing.Size(100, 13);
+            this.ipAddress.TabIndex = 34;
+            this.ipAddress.Text = "0.0.0.0";
+            this.ipAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(45, 196);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(481, 178);
+            this.txtLog.TabIndex = 33;
+            // 
+            // lbIp
+            // 
+            this.lbIp.AutoSize = true;
+            this.lbIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIp.Location = new System.Drawing.Point(61, 48);
+            this.lbIp.Name = "lbIp";
+            this.lbIp.Size = new System.Drawing.Size(95, 13);
+            this.lbIp.TabIndex = 32;
+            this.lbIp.Text = "Server IP Address:";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripDropDownButton1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip2.Size = new System.Drawing.Size(565, 25);
+            this.toolStrip2.TabIndex = 76;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem1,
+            this.saveConfigurationToolStripMenuItem,
+            this.loadConfigurationToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(38, 22);
+            this.toolStripButton2.Text = "File";
+            // 
+            // settingsToolStripMenuItem1
+            // 
+            this.settingsToolStripMenuItem1.Name = "settingsToolStripMenuItem1";
+            this.settingsToolStripMenuItem1.Size = new System.Drawing.Size(177, 22);
+            this.settingsToolStripMenuItem1.Text = "Settings";
+            this.settingsToolStripMenuItem1.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // saveConfigurationToolStripMenuItem
+            // 
+            this.saveConfigurationToolStripMenuItem.Name = "saveConfigurationToolStripMenuItem";
+            this.saveConfigurationToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.saveConfigurationToolStripMenuItem.Text = "Save Configuration";
+            // 
+            // loadConfigurationToolStripMenuItem
+            // 
+            this.loadConfigurationToolStripMenuItem.Name = "loadConfigurationToolStripMenuItem";
+            this.loadConfigurationToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.loadConfigurationToolStripMenuItem.Text = "Load Configuration";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createUserToolStripMenuItem,
+            this.deleteUserToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(48, 22);
+            this.toolStripDropDownButton1.Text = "Tools";
+            // 
+            // createUserToolStripMenuItem
+            // 
+            this.createUserToolStripMenuItem.Name = "createUserToolStripMenuItem";
+            this.createUserToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.createUserToolStripMenuItem.Text = "Create User";
+            this.createUserToolStripMenuItem.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // deleteUserToolStripMenuItem
+            // 
+            this.deleteUserToolStripMenuItem.Name = "deleteUserToolStripMenuItem";
+            this.deleteUserToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteUserToolStripMenuItem.Text = "Delete User";
+            this.deleteUserToolStripMenuItem.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 414);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtChatPort);
+            this.Controls.Add(this.ipAddress);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lbIp);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "MainForm";
+            this.Text = "Genome Server Normal";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Controls.SetChildIndex(this.btnSelectFile, 0);
+            this.Controls.SetChildIndex(this.btnStartServer, 0);
+            this.Controls.SetChildIndex(this.groupBox2, 0);
+            this.Controls.SetChildIndex(this.lbIp, 0);
+            this.Controls.SetChildIndex(this.txtLog, 0);
+            this.Controls.SetChildIndex(this.ipAddress, 0);
+            this.Controls.SetChildIndex(this.txtChatPort, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.toolStrip2, 0);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdModifiedHeaders;
+        private System.Windows.Forms.RadioButton rdStandardHeaders;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChatPort;
+        private System.Windows.Forms.TextBox ipAddress;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lbIp;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripButton2;
+        private System.Windows.Forms.ToolStripMenuItem saveConfigurationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadConfigurationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem createUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem1;
+    }
+}
+
---
+++ Genome-Server-Normal/MainForm.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using ServerClient;
+using FileSharingAppServer;
+using FileSharingApp;
+using System.Net.Sockets;
+
+namespace httpMethodsApp
+{
+    public enum encodingType { Origin, TwoBitEncoding, HuffmanWithSampling, HuffmanWithoutSampling, Huffman };
+    public partial class MainForm : Base
+    {
+        private string filesDirectory = "";
+        private HttpServerController httpServerController;
+        private bool useStandardHeaders = true;
+        public delegate void FileRecievedEventHandler(object source, string fileName);
+        private delegate void UpdateStatusCallback(string strMessage);
+        DatabaseCon UserDB = new DatabaseCon();
+
+
+        public MainForm()
+        {
+            UserDB.CreateDB();
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"ReceivedFiles";
+            MessageBox.Show(path);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            InitializeComponent();
+            
+        }
+
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.filesDirectory = folderBrowser.SelectedPath;
+                if (httpServerController != null)
+                {
+                    httpServerController._httpServer.includingPath = this.filesDirectory;
+                }
+
+            }
+        }
+
+      
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            setUPServer();
+
+        }
+
+        /*---------------------------------------CHAT APP--------------------------------------------*/
+
+
+
+
+
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+            Process.GetCurrentProcess().Kill();
+
+        }
+
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Process.GetCurrentProcess().Kill();
+
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            Form CreateUser = new Form();
+            CreateUser.StartPosition = FormStartPosition.CenterScreen;
+            CreateUser.FormBorderStyle = FormBorderStyle.FixedDialog;
+            CreateUser.Width = 400;
+            CreateUser.Height = 200;
+            CreateUser.Text = "Create User";
+            Label userLabel = new Label() { Left = 50, Top = 15, Text = "Username:" };
+            Label passLabel = new Label() { Left = 50, Top = 70, Text = "Password:" };
+
+            TextBox username = new TextBox { Left = 50, Top = 40, Width = 300 };
+            TextBox password = new TextBox { Left = 50, Top = 95, Width = 300 };
+            Button confirmation = new Button() { Text = "Ok", Left = 250, Width = 100, Top = 130 };
+            confirmation.Click += (sender2, d) => {
+                UserDB.CreateUser(username.Text, password.Text);
+                CreateUser.Close();
+            };
+            CreateUser.Controls.Add(username);
+            CreateUser.Controls.Add(password);
+            CreateUser.Controls.Add(confirmation);
+            CreateUser.Controls.Add(userLabel);
+            CreateUser.Controls.Add(passLabel);
+            CreateUser.ShowDialog();
+
+        }
+
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            Form CreateUser = new Form();
+            CreateUser.StartPosition = FormStartPosition.CenterScreen;
+            CreateUser.FormBorderStyle = FormBorderStyle.FixedDialog;
+            CreateUser.Width = 400;
+            CreateUser.Height = 150;
+            CreateUser.Text = "Delete User";
+            Label userLabel = new Label() { Left = 50, Top = 15, Text = "Username:" };
+
+            TextBox username = new TextBox { Left = 50, Top = 40, Width = 300 };
+            Button confirmation = new Button() { Text = "Ok", Left = 250, Width = 100, Top = 70 };
+            confirmation.Click += (sender2, d) => {
+                UserDB.DeleteUser(username.Text);
+                CreateUser.Close();
+            };
+            CreateUser.Controls.Add(username);
+            CreateUser.Controls.Add(confirmation);
+            CreateUser.Controls.Add(userLabel);
+            CreateUser.ShowDialog();
+        }
+
+
+        private void btnListen_Click()
+        {
+            //int port = int.Parse(txtFilePort.Text);
+
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("10.0.2.4", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            ipAddress.Text = localIP;
+            // Parse the server's IP address out of the TextBox
+            IPAddress ipAddr = IPAddress.Parse(localIP);
+            // Create a new instance of the ChatServer object
+
+            Debug.WriteLine(localIP);
+
+            FileSharingAppServer.ChatServer mainServer = new FileSharingAppServer.ChatServer(ipAddr);
+            // Hook the StatusChanged event handler to mainServer_StatusChanged
+            global::FileSharingAppServer.ChatServer.StatusChanged += new StatusChangedEventHandler(mainServer_StatusChanged);
+            // Start listening for connections
+            mainServer.StartListening(txtChatPort.Text);
+            // Show that we started to listen for connections
+            txtLog.AppendText("Monitoring for connections...\r\n");
+
+        }
+
+        public void mainServer_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            // Call the method that updates the form
+            this.Invoke(new UpdateStatusCallback(this.UpdateStatus), new object[] { e.EventMessage });
+        }
+
+        private void UpdateStatus(string strMessage)
+        {
+            // Updates the log with the message
+            txtLog.AppendText(strMessage + "\r\n");
+        }
+
+
+
+
+        /*---------------------------------------GTTP CODE--------------------------------------------*/
+
+        private void setUPServer()
+        {
+            
+            httpServerController = new HttpServerController(AppSettings.Port);
+            httpServerController._httpServer.useStandardHeaders = this.useStandardHeaders;
+
+            if (filesDirectory != "")
+            {
+                httpServerController._httpServer.includingPath = Path.GetDirectoryName(filesDirectory);
+            }
+        }
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+
+            if (btnStartServer.Text == "Start Server")
+            {
+
+                if (filesDirectory != "")
+                {
+                    httpServerController._httpServer.includingPath = filesDirectory;
+                }
+                httpServerController._httpServer.startedListeningCallback = new StartedListening(ServerhasStarted);
+
+                this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+                httpServerController.start();
+                btnListen_Click();
+
+            }
+            else
+            {
+                httpServerController.stop();
+                btnStartServer.Text = "Start Server";
+
+                MessageBox.Show("The Server has stopped");
+
+
+            }
+
+
+
+        }
+        private void ServerhasStarted(bool started, String errorMessage)
+        {
+            if (InvokeRequired)
+            {
+
+
+                this.Invoke((MethodInvoker)delegate()
+                {
+                    this.Cursor = System.Windows.Forms.Cursors.Default;
+
+                    if (started)
+                    {
+                        btnStartServer.Text = "Stop Server";
+
+                        MessageBox.Show("The Server has started");
+                    }
+                    else
+                    {
+                        MessageBox.Show(errorMessage);
+                    }
+
+                });
+            }
+        }
+        
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (httpServerController != null)
+            {
+                httpServerController.stop();
+            }
+        }
+
+
+        private void rdStandardHeaders_CheckedChanged(object sender, EventArgs e)
+        {
+            if( rdStandardHeaders.Checked ) useStandardHeaders = true;
+        }
+
+        private void rdModifiedHeaders_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdModifiedHeaders.Checked) useStandardHeaders = false;
+        }
+
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AppSettingsForm appSettingForm = new AppSettingsForm();
+            if (appSettingForm.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                httpServerController._httpServer.PortNumber = AppSettings.Port;
+            }
+        }
+    }
+}
+
+
+
+
